aba518a513d17f941f037271c824d85492ef3e38
trieucaoduy <trieucaoduy98@gmail.com>
2019-10-23 23:45:47 +0700

    upload new file *

--- app/src/main/java/com/example/furniture/LoginActivity.java
+++ app/src/main/java/com/example/furniture/LoginActivity.java
@@ -4,22 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
+    private TextView txtSignUp;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actitvity_login);
         btnLogin = findViewById(R.id.btnLogin);
+        txtSignUp = findViewById(R.id.txtSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/furniture/SignupActivity.java
+++ app/src/main/java/com/example/furniture/SignupActivity.java
@@ -1,14 +1,40 @@
 package com.example.furniture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+    private Button btnSignUp;
+    private TextView txtLogin;
+    private Intent intent;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtLogin = findViewById(R.id.txtLogin);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
