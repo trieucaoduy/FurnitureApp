2f263bd22058cc81cc827fcf3cea4349869ddb9e
trieucaoduy <trieucaoduy98@gmail.com>
2019-10-25 23:55:13 +0700

    upload new file *

---
+++ app/src/main/java/com/example/furniture/HomeFragment.java
@@ -0,0 +1,89 @@
+package com.example.furniture;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+    ViewFlipper v_flipper;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        //viewflipper
+        int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+        v_flipper = (ViewFlipper) view.findViewById(R.id.v_flipper);
+
+
+        //for loop
+        /*for (int i = 0; i < images.length; i++) {
+            flipperImages(images[i];
+        }
+        */
+
+        //loop
+        for (int image : images) {
+            flipperImages(image);
+        }
+
+
+        //Recylerview + CardView
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.product_1, "Green Corner", "1000$"));
+        exampleList.add(new ExampleItem(R.drawable.product_2, "Laura", "700$"));
+        exampleList.add(new ExampleItem(R.drawable.product_3, "Nude", "900$"));
+        exampleList.add(new ExampleItem(R.drawable.product_4, "Arora", "550$"));
+
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+
+            }
+        });
+
+
+        return view;
+    }
+
+    public void flipperImages(int image) {
+        Log.i("Set flipper called", image+"");
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(3000);
+        v_flipper.setAutoStart(true);
+
+        //animation
+        v_flipper.setInAnimation(getContext(), android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(getContext(), android.R.anim.slide_out_right);
+    }
+}
--- app/src/main/java/com/example/furniture/MainActivity.java
+++ app/src/main/java/com/example/furniture/MainActivity.java
@@ -26,12 +26,8 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    ViewFlipper v_flipper;
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,48 +42,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //Drawerview Navigation
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        //viewflipper
-
-        int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
-        v_flipper = findViewById(R.id.v_flipper);
-
-        //for loop
-        /*for (int i = 0; i < images.length; i++) {
-            flipperImages(images[i];
-        }
-        */
-
-        //loop
-        for (int image : images) {
-            flipperImages(image);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
         }
 
-        //Recylerview + CardView
-        ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.product_1, "Green Corner", "1000$"));
-        exampleList.add(new ExampleItem(R.drawable.product_2, "Laura", "700$"));
-        exampleList.add(new ExampleItem(R.drawable.product_3, "Nude", "900$"));
-        exampleList.add(new ExampleItem(R.drawable.product_4, "Arora", "550$"));
 
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleList);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-
-        mRecyclerView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-
-            }
-        });
 
     }
 
@@ -96,12 +63,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         switch (item.getItemId()) {
 
+            case R.id.home: {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            }
+
             case R.id.profile: {
-                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
                 break;
             }
             case R.id.cart: {
-                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
                 break;
             }
             case R.id.order: {
@@ -140,19 +112,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_menu, menu);
         return true;
-    }
-
-    public void flipperImages(int image) {
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(image);
-
-        v_flipper.addView(imageView);
-        v_flipper.setFlipInterval(3000);
-        v_flipper.setAutoStart(true);
-
-        //animation
-        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
-        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 
 
